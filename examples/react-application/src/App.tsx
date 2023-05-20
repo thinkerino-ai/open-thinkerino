b223b3f802e98c2468f626ccbf301eb19401a2ca
Daniele <dRain88@gmail.com>
2023-05-20 02:37:37 +0200

    turns out @OctoD was right once again <.<

--- examples/react-application/src/App.tsx
+++ examples/react-application/src/App.tsx
@@ -2,8 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { helloAPI } from 'thinkerino'
-import { runTest } from 'thinkerino/dist'
+import { helloAPI, runTest } from 'thinkerino'
 
 function App() {
   const [count, setCount] = useState(0)
--- thinkerino.js/src/index.ts
+++ thinkerino.js/src/index.ts
@@ -5,6 +5,7 @@ export const helloAPI: (name: string) => string = hello
 
 export const runTest = test
 
+// TODO fix typing
 export const makeExpr = expr => {
     if (Array.isArray(expr))
         return expr.map(makeExpr)
